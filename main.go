e2d6ff11c5315610274906ac3fcdc39e86008a6b
Kai Matolat <kaimatolat@hotmail.com>
2024-09-24 23:25:55 +1000

    deleted posts.go to eliminate confusion

--- main.go
+++ main.go
@@ -11,8 +11,12 @@ func main() {
 	})
 	
 	http.HandleFunc("/posts", func(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the posts page!")
-})
+		fmt.Fprintf(w, "Welcome to the posts page!")
+	})
+	
+	http.HandleFunc("/art", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "This is where art I like will appear")
+	})
 	
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		fmt.Println("Server error:", err)
